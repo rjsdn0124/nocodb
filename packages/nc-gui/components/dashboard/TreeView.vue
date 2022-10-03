cbd6a043d86f8ae45688d1e30f3ec091520f16d5
mertmit <mertmit99@gmail.com>
2022-10-03 22:14:13 +0300

    fix: rename table
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nc-gui/components/dashboard/TreeView.vue
+++ packages/nc-gui/components/dashboard/TreeView.vue
@@ -562,7 +562,7 @@ function openTableCreateDialog(baseId?: string) {
           <template v-if="contextMenuTarget.type === 'table'">
             <a-menu-item
               v-if="isUIAllowed('table-rename')"
-              @click="openRenameTableDialog(contextMenuTarget.value, base.id, true)"
+              @click="openRenameTableDialog(contextMenuTarget.value, bases[0].id, true)"
             >
               <div class="nc-project-menu-item">
                 {{ $t('general.rename') }}
