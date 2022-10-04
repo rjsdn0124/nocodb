28dbc17267c997b5d7c46188534559a8941490b1
mertmit <mertmit99@gmail.com>
2022-10-05 01:52:23 +0300

    fix(at-import): handle more than 64 options for mysql
    Signed-off-by: mertmit <mertmit99@gmail.com>

--- packages/nocodb/src/lib/meta/api/sync/helpers/job.ts
+++ packages/nocodb/src/lib/meta/api/sync/helpers/job.ts
@@ -577,9 +577,16 @@ export default async (
             ncCol.colOptions = {
               options: [...colOptions.data],
             };
-            // if options are empty, configure '' as default option
-            ncCol.dtxp =
-              colOptions.data.map((el) => `'${el.title.replace(/'/gi, "''")}'`).join(',') || "''";
+
+            if (['mysql', 'mysql2'].includes(getRootDbType())) {
+              if (colOptions.data.length > 63) {
+                ncCol.dt = 'text';
+              } else {
+                // if options are empty, configure '' as an option
+                ncCol.dtxp = colOptions.data.map((el) => `'${el.title.replace(/'/gi, "''")}'`).join(',') || "''";
+              }
+            }
+            
             break;
           case undefined:
             break;
